678908c97a47647f833cc8dff5c927f0918fb721
pram <pram@attale.com>
2022-04-15 19:18:38 +0100

    update test

--- src/test/java/com/naughtyzombie/springbootswing/SpringBootSwingApplicationTests.java
+++ src/test/java/com/naughtyzombie/springbootswing/SpringBootSwingApplicationTests.java
@@ -1,10 +1,16 @@
 package com.naughtyzombie.springbootswing;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootSwingApplicationTests {
+
+    @BeforeAll
+    public static void setupHeadlessMode() {
+        System.setProperty("java.awt.headless", "false");
+    }
 
     @Test
     void contextLoads() {
